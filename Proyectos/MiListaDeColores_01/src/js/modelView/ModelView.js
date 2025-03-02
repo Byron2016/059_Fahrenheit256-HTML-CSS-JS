0742749e95951cc0077347c672440d36220d7abb
Byron2016 <bgva2005@yahoo.com>
2025-03-02 13:44:41 -0500

    feat: parte_04-b_method

--- Proyectos/MiListaDeColores_01/src/js/modelView/ModelView.js
+++ Proyectos/MiListaDeColores_01/src/js/modelView/ModelView.js
@@ -15,7 +15,16 @@ const ModelViewConsole = function (model) {
     reload: function (data) {
       console.log("data [ModelViewConsole-reload]", data);
       const print = ConvertArrayToString(data);
-      console.log(`%c${print}`, `color: Yellow; font-size: 14px;`);
+      // console.log(`%c${print}`, `color: Yellow; font-size: 14px;`);
+      console.log(`%cLista : ${print}`, `color: Yellow; font-size: 14px;`);
+    },
+    reloadMethod: function (data, nameMethod, method) {
+      const result = method(data);
+      const print = ConvertArrayToString(result);
+      console.log(
+        `%c${nameMethod} : ${print}`,
+        `color: Yellow; font-size: 14px;`
+      );
     },
   };
 };
--- Proyectos/MiListaDeColores_01/src/js/observer/Observers.js
+++ Proyectos/MiListaDeColores_01/src/js/observer/Observers.js
@@ -10,3 +10,18 @@ const Observer = function (modelView) {
     },
   };
 };
+
+const ObserverMethod = function (modelView, methodName, method) {
+  //Private
+  const _modelView = modelView;
+  const _methodName = methodName;
+  const _method = method;
+
+  //Public
+  return {
+    update: function (data, event) {
+      console.log(`En update del ObserverMethod ${event}`);
+      _modelView.reloadMethod(data, _methodName, _method);
+    },
+  };
+};
